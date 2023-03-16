62b924841f87234932762fdfaf0fcd3ac8e15bdb
Barry LaFond <blafond@redhat.com>
2023-03-16 15:14:52 -0500

    reactive examples compatible with 6.2 ORM

---
+++ examples/native-sql-example/src/main/java/org/hibernate/reactive/example/nativesql/Author.java
@@ -0,0 +1,55 @@
+/* Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright: Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.reactive.example.nativesql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLInsert;
+import org.hibernate.annotations.SQLUpdate;
+import org.hibernate.annotations.Subselect;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Subselect("select name, id from authors")
+@SQLInsert(sql = "insert into authors (name, id) values ($1, $2)")
+@SQLUpdate(sql = "")
+@SQLDelete(sql = "delete from authors where id = $1")
+class Author {
+	@Id @GeneratedValue
+	private Integer id;
+
+	@NotNull @Size(max=100)
+	private String name;
+
+	@OneToMany(mappedBy = "author")
+	private List<Book> books = new ArrayList<>();
+
+	Author(String name) {
+		this.name = name;
+	}
+
+	Author() {}
+
+	Integer getId() {
+		return id;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	List<Book> getBooks() {
+		return books;
+	}
+}
