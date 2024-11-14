28768e001a4b4a25ec1932a7340a00afa5259c40
Davide D'Alto <davide@hibernate.org>
2024-11-14 15:15:13 +0100

    Update DynamicEntityTest

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/dynamic/DynamicEntityTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/dynamic/DynamicEntityTest.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.cfg.Configuration;
 import org.hibernate.reactive.BaseReactiveTest;
 
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,11 @@ import io.vertx.junit5.VertxTestContext;
 import static java.util.Map.entry;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.metamodel.internal.AbstractDynamicMapInstantiator.TYPE_KEY;
 
+/**
+ * Copied from Hibernate ORM: org.hibernate.orm.test.mapping.dynamic.DynamicEntityTest
+ */
 @Timeout(value = 10, timeUnit = MINUTES)
 
 public class DynamicEntityTest extends BaseReactiveTest {
@@ -28,6 +33,12 @@ public class DynamicEntityTest extends BaseReactiveTest {
 	@Override
 	protected Collection<String> mappings() {
 		return List.of( "org/hibernate/reactive/dynamic/Book.hbm.xml" );
+	}
+
+	@Override
+	protected Configuration constructConfiguration() {
+		return super.constructConfiguration()
+				.setProperty( "hibernate.default_entity_mode", "dynamic-map" );
 	}
 
 	@Test
@@ -41,13 +52,12 @@ public class DynamicEntityTest extends BaseReactiveTest {
 				.withTransaction( session -> session.persist( "Book", book ) )
 				.chain( v -> getMutinySessionFactory()
 						.withSession( session -> session.createSelectionQuery( "from Book", Map.class ).getSingleResult() )
-						.invoke( map -> assertThat( map ).containsExactly(
+						.invoke( map -> assertThat( map.entrySet() ).containsExactlyInAnyOrder(
 								entry( "author", "Christian Bauer and Gavin King" ),
 								entry( "ISBN", "9781932394153" ),
 								entry( "title", "Hibernate in Action" ),
-								entry( "author", "Christian Bauer and Gavin King" )
+								entry( TYPE_KEY, "Book" )
 						) ) )
 		);
 	}
-
 }
