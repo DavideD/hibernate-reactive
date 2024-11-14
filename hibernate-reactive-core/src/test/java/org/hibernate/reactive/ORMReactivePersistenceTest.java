3e376532a42f3fcf1f10b87935faba87685e078d
Davide D'Alto <davide@hibernate.org>
2024-11-14 15:13:09 +0100

    Revert changes to ORMReactivePersistentTest

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/ORMReactivePersistenceTest.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/ORMReactivePersistenceTest.java
@@ -6,9 +6,8 @@
 package org.hibernate.reactive;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -22,6 +21,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxTestContext;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.COCKROACHDB;
@@ -29,6 +32,7 @@ import static org.hibernate.reactive.containers.DatabaseConfiguration.DBType.DB2
 import static org.hibernate.reactive.containers.DatabaseConfiguration.dbType;
 import static org.hibernate.reactive.provider.Settings.DIALECT;
 import static org.hibernate.reactive.provider.Settings.DRIVER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Timeout(value = 10, timeUnit = MINUTES)
 
@@ -43,8 +47,8 @@ public class ORMReactivePersistenceTest extends BaseReactiveTest {
 	private SessionFactory ormFactory;
 
 	@Override
-	protected Collection<String> mappings() {
-		return List.of( "org/hibernate/reactive/dynamic/Book.hbm.xml" );
+	protected Collection<Class<?>> annotatedEntities() {
+		return List.of( Flour.class );
 	}
 
 	@BeforeEach
@@ -52,7 +56,6 @@ public class ORMReactivePersistenceTest extends BaseReactiveTest {
 		Configuration configuration = constructConfiguration();
 		configuration.setProperty( DRIVER, dbType().getJdbcDriver() );
 		configuration.setProperty( DIALECT, dbType().getDialectClass().getName() );
-		configuration.setProperty( "hibernate.default_entity_mode", "dynamic-map" );
 
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
 				.applySettings( configuration.getProperties() );
@@ -66,19 +69,113 @@ public class ORMReactivePersistenceTest extends BaseReactiveTest {
 		ormFactory.close();
 	}
 
+	@Test
+	public void testORMWithStageSession(VertxTestContext context) {
+		final Flour almond = new Flour( 1, "Almond", "made from ground almonds.", "Gluten free" );
+
+		try (Session session = ormFactory.openSession()) {
+			session.beginTransaction();
+			session.persist( almond );
+			session.getTransaction().commit();
+		}
+
+		// Check database with Stage session and verify 'almond' flour exists
+		test( context, openSession()
+				.thenCompose( stageSession -> stageSession.find( Flour.class, almond.id ) )
+				.thenAccept( entityFound -> assertEquals( almond, entityFound ) )
+		);
+	}
 
 	@Test
-	public void test() {
-		Map<String, String> book = new HashMap<>();
-		book.put( "ISBN", "9781932394153" );
-		book.put( "title", "Hibernate in Action" );
-		book.put( "author", "Christian Bauer and Gavin King" );
-
+	public void testORMWitMutinySession(VertxTestContext context) {
+		final Flour rose = new Flour( 2, "Rose", "made from ground rose pedals.", "Full fragrance" );
 
 		try (Session ormSession = ormFactory.openSession()) {
 			ormSession.beginTransaction();
-			ormSession.persist( "Book", book );
+			ormSession.persist( rose );
 			ormSession.getTransaction().commit();
+		}
+
+		// Check database with Mutiny session and verify 'rose' flour exists
+		test( context, openMutinySession()
+				.chain( session -> session.find( Flour.class, rose.id ) )
+				.invoke( foundRose -> assertEquals( rose, foundRose ) )
+		);
+	}
+
+	@Entity(name = "Flour")
+	@Table(name = "Flour")
+	public static class Flour {
+		@Id
+		private Integer id;
+		private String name;
+		private String description;
+		private String type;
+
+		public Flour() {
+		}
+
+		public Flour(Integer id, String name, String description, String type) {
+			this.id = id;
+			this.name = name;
+			this.description = description;
+			this.type = type;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if ( this == o ) {
+				return true;
+			}
+			if ( o == null || getClass() != o.getClass() ) {
+				return false;
+			}
+			Flour flour = (Flour) o;
+			return Objects.equals( name, flour.name ) &&
+					Objects.equals( description, flour.description ) &&
+					Objects.equals( type, flour.type );
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash( name, description, type );
 		}
 	}
 }
