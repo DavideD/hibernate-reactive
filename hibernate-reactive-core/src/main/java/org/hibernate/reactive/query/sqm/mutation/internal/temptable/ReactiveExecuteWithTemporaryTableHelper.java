4cc6be96b8e426d68111e8db093491c6763d4649
Davide D'Alto <davide@hibernate.org>
2025-08-14 17:37:14 +0200

    Remove unused methods

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/temptable/ReactiveExecuteWithTemporaryTableHelper.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/query/sqm/mutation/internal/temptable/ReactiveExecuteWithTemporaryTableHelper.java
@@ -13,21 +13,14 @@ import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
 
-import org.hibernate.LockMode;
-import org.hibernate.LockOptions;
 import org.hibernate.boot.TempTableDdlTransactionHandling;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.temptable.TemporaryTable;
 import org.hibernate.dialect.temptable.TemporaryTableSessionUidColumn;
 import org.hibernate.dialect.temptable.TemporaryTableStrategy;
-import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.hibernate.engine.jdbc.spi.JdbcCoordinator;
-import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.metamodel.mapping.EntityMappingType;
-import org.hibernate.metamodel.mapping.ModelPart;
-import org.hibernate.query.sqm.mutation.internal.temptable.ExecuteWithTemporaryTableHelper;
 import org.hibernate.query.sqm.mutation.spi.AfterUseAction;
 import org.hibernate.query.sqm.mutation.spi.BeforeUseAction;
 import org.hibernate.reactive.logging.impl.Log;
@@ -37,12 +30,6 @@ import org.hibernate.reactive.query.sqm.mutation.internal.temptable.ReactiveTemp
 import org.hibernate.reactive.session.ReactiveConnectionSupplier;
 import org.hibernate.reactive.sql.exec.internal.StandardReactiveJdbcMutationExecutor;
 import org.hibernate.reactive.util.impl.CompletionStages;
-import org.hibernate.sql.ast.SqlAstJoinType;
-import org.hibernate.sql.ast.SqlAstTranslatorFactory;
-import org.hibernate.sql.ast.tree.expression.JdbcParameter;
-import org.hibernate.sql.ast.tree.insert.InsertSelectStatement;
-import org.hibernate.sql.ast.tree.select.QueryPart;
-import org.hibernate.sql.ast.tree.select.QuerySpec;
 import org.hibernate.sql.exec.spi.ExecutionContext;
 import org.hibernate.sql.exec.spi.JdbcOperationQueryMutation;
 import org.hibernate.sql.exec.spi.JdbcParameterBindings;
@@ -63,40 +50,6 @@ public final class ReactiveExecuteWithTemporaryTableHelper {
 	}
 
 	public static CompletionStage<Integer> saveIntoTemporaryTable(
-			InsertSelectStatement temporaryTableInsert,
-			JdbcParameterBindings jdbcParameterBindings,
-			ExecutionContext executionContext) {
-		final SessionFactoryImplementor factory = executionContext.getSession().getFactory();
-		final JdbcServices jdbcServices = factory.getJdbcServices();
-		final JdbcEnvironment jdbcEnvironment = jdbcServices.getJdbcEnvironment();
-		final SqlAstTranslatorFactory sqlAstTranslatorFactory = jdbcEnvironment.getSqlAstTranslatorFactory();
-		final LockOptions lockOptions = executionContext.getQueryOptions().getLockOptions();
-		final LockMode lockMode = lockOptions.getLockMode();
-		// Acquire a WRITE lock for the rows that are about to be modified
-		lockOptions.setLockMode( LockMode.WRITE );
-		// Visit the table joins and reset the lock mode if we encounter OUTER joins that are not supported
-		final QueryPart sourceSelectStatement = temporaryTableInsert.getSourceSelectStatement();
-		if ( sourceSelectStatement != null
-				&& !jdbcEnvironment.getDialect().supportsOuterJoinForUpdate() ) {
-			sourceSelectStatement.visitQuerySpecs(
-					querySpec -> querySpec.getFromClause().visitTableJoins(
-								tableJoin -> {
-									if ( tableJoin.isInitialized()
-											&& tableJoin.getJoinType() != SqlAstJoinType.INNER ) {
-										lockOptions.setLockMode( lockMode );
-									}
-								}
-						)
-			);
-		}
-		final JdbcOperationQueryMutation jdbcInsert = sqlAstTranslatorFactory.buildMutationTranslator( factory, temporaryTableInsert )
-				.translate( jdbcParameterBindings, executionContext.getQueryOptions() );
-		lockOptions.setLockMode( lockMode );
-
-		return saveIntoTemporaryTable(jdbcInsert, jdbcParameterBindings, executionContext);
-	}
-
-	public static CompletionStage<Integer> saveIntoTemporaryTable(
 			JdbcOperationQueryMutation jdbcInsert,
 			JdbcParameterBindings jdbcParameterBindings,
 			ExecutionContext executionContext) {
@@ -109,23 +62,6 @@ public final class ReactiveExecuteWithTemporaryTableHelper {
 						(integer, preparedStatement) -> {},
 						executionContext
 				);
-	}
-
-	public static QuerySpec createIdTableSelectQuerySpec(
-			TemporaryTable idTable,
-			JdbcParameter sessionUidParameter,
-			EntityMappingType entityDescriptor,
-			ExecutionContext executionContext) {
-		return createIdTableSelectQuerySpec( idTable, null, sessionUidParameter, entityDescriptor, executionContext );
-	}
-
-	public static QuerySpec createIdTableSelectQuerySpec(
-			TemporaryTable idTable,
-			ModelPart fkModelPart,
-			JdbcParameter sessionUidParameter,
-			EntityMappingType entityDescriptor,
-			ExecutionContext executionContext) {
-		return ExecuteWithTemporaryTableHelper.createIdTableSelectQuerySpec( idTable, fkModelPart, sessionUidParameter, entityDescriptor, executionContext );
 	}
 
 	@Deprecated(forRemoval = true, since = "3.1")
